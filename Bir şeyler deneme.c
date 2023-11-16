0a0bd7be8f597f30bb6282c774306cc8ae9b18f1
Oylesi <84276348+Oylesi@users.noreply.github.com>
2023-11-16 12:46:52 +0300

    Added some project

---
+++ Bir şeyler deneme.c
@@ -0,0 +1,30 @@
+﻿// Bir şeyler deneme.c
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3;
+
+    for (num1 = 10; num1 >= 0; num1--)
+    {
+        for (num2 = 0; num2 <= 10+num1; num2++)
+        {
+            if (num2 >= num1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+
+        }
+        printf("\n");
+    }
+
+    
+    return 0;
+}
+
